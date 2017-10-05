5a351d661aae55c103f8da03055cbdf1a08f5d27
MaksPrime <mrdancerize@gmail.com>
2017-10-05 09:07:42 +0300

    2.2.1

--- HW 2.2 Documents/HW_2.2.4/script.js
+++ HW 2.2 Documents/HW_2.2.4/script.js
@@ -1,7 +1,16 @@
 var Select = {};
-
 Select.par = function () {
-document.getElementsByTagName("p").onclick = function () {
-    
+    var paragraphs = document.getElementsByTagName("p");
+    for (i=0; i<paragraphs.length; i++) {paragraphs[i].style.border = "solid 1px black";}
 }
+// document.getElementById(firstInp).onclick = Select.par();
+
+Select.hrs = function () {
+    var hrefs = document.getElementsByTagName("a");
+    for (i=0; i<hrefs.length; i++) {hrefs[i].style.border = "solid 2px red";}
+}
+
+Select.block = function () {
+    var div = document.getElementById("test_div");
+    div.style.border = "solid 2px green";
 }
