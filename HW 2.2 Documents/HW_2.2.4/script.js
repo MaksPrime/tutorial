800b49ebb60c563a0301942022759e922c778fbe
MaksPrime <mrdancerize@gmail.com>
2017-10-07 20:27:05 +0300

    finished task 2.2.4

--- HW 2.2 Documents/HW_2.2.4/script.js
+++ HW 2.2 Documents/HW_2.2.4/script.js
@@ -3,14 +3,22 @@ Select.par = function () {
     var paragraphs = document.getElementsByTagName("p");
     for (i=0; i<paragraphs.length; i++) {paragraphs[i].style.border = "solid 1px black";}
 }
-// document.getElementById(firstInp).onclick = Select.par();
+document.getElementById('firstInp').onclick = Select.par;
 
 Select.hrs = function () {
     var hrefs = document.getElementsByTagName("a");
     for (i=0; i<hrefs.length; i++) {hrefs[i].style.border = "solid 2px red";}
 }
+document.getElementById('secondInp').onclick = Select.hrs;
 
 Select.block = function () {
     var div = document.getElementById("test_div");
     div.style.border = "solid 2px green";
 }
+document.getElementById('thirdInp').onclick = Select.block;
+
+Select.reset = function () {
+    var allElements = document.getElementsByClassName('content');
+    for (i=0; i<allElements.length; i++) {allElements[i].style.border = 'none';}
+}
+document.getElementById('reset').onclick = Select.reset;
