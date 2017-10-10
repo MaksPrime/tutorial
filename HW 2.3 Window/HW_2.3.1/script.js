da6e433deb0f9bec8353f55765592ae9219800ee
Maksim <mrdancerize@gmail.com>
2017-10-10 17:40:11 +0400

    completed task 2.3

--- HW 2.3 Window/HW_2.3.1/script.js
+++ HW 2.3 Window/HW_2.3.1/script.js
@@ -1,19 +1,33 @@
 window.onload = function () {
-var counter = 0;
+var counter = 3597;
+var minutes = 0;
+var hours = 0;
 var interval;
 
 function count() {
-    get ('seconds').innerHTML = counter;
+    get ('seconds').innerHTML = ('0' + (counter%60)).slice(-2);
     counter++;
-}
+    
+    	if ((counter%60)==1){
+    		minutes = ((counter - counter%60)/60);
+    		get ('minutes').innerHTML = ('0' + (minutes%60)).slice(-2);
+    	}
+
+    		if((counter%3600)==1){
+				hours +=1;
+    			get ('hours').innerHTML = ('0' + hours).slice(-2);
+    		}
+    	}
 
 get('start').onclick = function () {
-    interval = setInterval(count, 2000);
+    interval = setInterval(count, 1000);
 }
 
 get('stop').onclick = function () {
     clearInterval(interval);
 }
+
+get('clear')
 }
 
 var get = function (id) {
