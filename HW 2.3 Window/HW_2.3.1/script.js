ece4166e58d27e9c001cc3ae5bfb517743c8c2ac
Maksim <mrdancerize@gmail.com>
2017-10-11 13:47:40 +0400

    final version of 2.3

--- HW 2.3 Window/HW_2.3.1/script.js
+++ HW 2.3 Window/HW_2.3.1/script.js
@@ -3,7 +3,16 @@ var counter = 3537;
 var minutes = 0;
 var hours = 0;
 var interval;
+var button = get('start'); 
 
+//мигалка
+setInterval(function(){
+    get('colon').innerHTML = ".";
+        setTimeout(function(){
+             get('colon').innerHTML = ":";}, 500)
+}, 1000)
+
+//алгоритм вывода времени:
 function count() {
     get ('seconds').innerHTML = ('0' + (counter%60)).slice(-2);
     counter++;
@@ -19,20 +28,20 @@ function count() {
     		}
     	}
 
-    var button = get('start');
+//поведение стартовой кнопки:    
+    button.onclick = startFunction;
 	function startFunction() {
+        if (button.getAttribute('id')=='start') {
     interval = setInterval(count, 1000);
     button.setAttribute('value', 'Stop');
     button.setAttribute('id', 'stop');
-        button.onclick = function () {
+    } else {
             clearInterval(interval);
             button.setAttribute('id', 'start');
-            button.setAttribute('value', 'Start');
+            button.setAttribute('value', 'Start');}
         }
-            }
-    button.onclick = startFunction;
 
-
+//поведение кнопки сброса:    
 get('clear').onclick = function () {
 	counter = 0;
 	minutes = 0;
@@ -44,6 +53,7 @@ get('clear').onclick = function () {
 
 }
 
+//упрощение получения элемента по id:
 var get = function (id) {
     return document.getElementById(id);
 }
